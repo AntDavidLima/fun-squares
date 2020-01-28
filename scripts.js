6b10d17f1a197d30eb1e72e6ce3901dafdedb9f5
David Lima <DavidLima@alu.ufc.br>
2020-01-28 20:09:48 -0300

    Change square color
    When you pass the cursor by a square it change  his color to a random color.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,23 @@
 const addSqrBtn = document.querySelector('div#controller button');
 const body = document.getElementById('squares');
 
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+  color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+ }
+
 addSqrBtn.onclick = () => {
   const square = document.createElement('div');
   square.setAttribute('class', 'sqr');
   square.style.background = '#f00';
   square.style.width = '100px';
   square.style.height = '100px';
+  square.onmouseover = () => {
+    square.style.backgroundColor = getRandomColor();
+  }
   body.appendChild(square);
 };
